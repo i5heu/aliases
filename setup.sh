5133c27a9c0ab94df7b0a8d9bf630ac4c057ab58
Mia Heidenstedt <i5heu@i5h.eu>
2024-06-12 11:20:14 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+lines="source .aliases/aliases"
+
+if ! grep -qF "$lines" "$HOME/.bashrc"; then
+    # Append the lines to .bashrc
+    echo "$lines" >> "$HOME/.bashrc"
+    echo "Lines successfully added to .bashrc"
+else
+    echo "Lines already exist in .bashrc"
+fi
