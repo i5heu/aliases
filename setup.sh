8e343d07ac24b07cb88a6b30f69a4a4982b2e8cf
Mia Heidenstedt <i5heu@i5h.eu>
2024-06-25 15:59:38 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 
-lines="source .aliases/aliases"
+lines="source \$HOME/.aliases/aliases"
 
-if ! grep -qF "$lines" "$HOME/.bashrc"; then
+if ! grep -qxF "$lines" "$HOME/.bashrc"; then
     # Append the lines to .bashrc
     echo "$lines" >> "$HOME/.bashrc"
     echo "Lines successfully added to .bashrc"
 else
     echo "Lines already exist in .bashrc"
+fi
+
+if [ -f "$HOME/.bash_profile" ]; then
+    profile="$HOME/.bash_profile"
+elif [ -f "$HOME/.profile" ]; then
+    profile="$HOME/.profile"
+fi
+
+if [ -n "$profile" ] && ! grep -qxF "source \$HOME/.bashrc" "$profile"; then
+    echo "source \$HOME/.bashrc" >> "$profile"
+    echo "Added sourcing of .bashrc to $profile"
+fi
+
+# Reload profile
+if [ -n "$profile" ]; then
+    source "$profile"
 fi
