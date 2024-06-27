06c020f69c38df48d1f1ef472773124914dee849
Mia Heidenstedt <i5heu@i5h.eu>
2024-06-27 16:29:18 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,14 +2,15 @@
 
 lines="source \$HOME/.aliases/aliases"
 
+# Update .bashrc if it doesn't already contain the source line
 if ! grep -qxF "$lines" "$HOME/.bashrc"; then
-    # Append the lines to .bashrc
     echo "$lines" >> "$HOME/.bashrc"
     echo "Lines successfully added to .bashrc"
 else
     echo "Lines already exist in .bashrc"
 fi
 
+# Update the appropriate profile file for bash
 if [ -f "$HOME/.bash_profile" ]; then
     profile="$HOME/.bash_profile"
 elif [ -f "$HOME/.profile" ]; then
@@ -21,7 +22,33 @@ if [ -n "$profile" ] && ! grep -qxF "source \$HOME/.bashrc" "$profile"; then
     echo "Added sourcing of .bashrc to $profile"
 fi
 
-# Reload profile
+# Reload bash profile
 if [ -n "$profile" ]; then
     source "$profile"
+fi
+
+# Check if the user is using zsh
+if [ -n "$ZSH_VERSION" ]; then
+    # Update .zshrc if it doesn't already contain the source line
+    if ! grep -qxF "$lines" "$HOME/.zshrc"; then
+        echo "$lines" >> "$HOME/.zshrc"
+        echo "Lines successfully added to .zshrc"
+    else
+        echo "Lines already exist in .zshrc"
+    fi
+
+    # Update the appropriate profile file for zsh
+    if [ -f "$HOME/.zprofile" ]; then
+        zprofile="$HOME/.zprofile"
+    fi
+
+    if [ -n "$zprofile" ] && ! grep -qxF "source \$HOME/.zshrc" "$zprofile"; then
+        echo "source \$HOME/.zshrc" >> "$zprofile"
+        echo "Added sourcing of .zshrc to $zprofile"
+    fi
+
+    # Reload zsh profile
+    if [ -n "$zprofile" ]; then
+        source "$zprofile"
+    fi
 fi
