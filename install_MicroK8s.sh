7914536038cda292ec0f84e95236047cb795e150
Mia Heidenstedt <i5heu@i5h.eu>
2024-07-03 12:42:16 +0200

    Update install_MicroK8s.sh

--- install_MicroK8s.sh
+++ install_MicroK8s.sh
@@ -6,11 +6,14 @@ sudo apt full-upgrade -y
 
 # Check if microk8s is already installed and install it if not
 if snap list microk8s 1>/dev/null 2>&1; then
-    echo "microk8s is already installed."
-else
-    echo "microk8s is not installed, attempting to install..."
-    sudo snap install microk8s --classic
+    echo "microk8s is already installed... removing"
+    sudo snap remove microk8s
 fi
+
+sudo mkdir -p /var/snap/microk8s/common/
+sudo cp $HOME/.aliases/microk8s_config.yaml /var/snap/microk8s/common/.microk8s.yaml
+
+sudo snap install microk8s --classic
 
 # Add the current user to the microk8s group and change ownership of .kube directory
 sudo usermod -a -G microk8s $USER
